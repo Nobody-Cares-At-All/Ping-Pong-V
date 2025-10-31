9480ed9b5d053d7d4f698d18efbb3acd78f6bc0e
Nobody-Cares-At-All <isaacmarques2765@gmail.com>
2025-10-31 09:52:26 +0000

    gif kakaka

--- app/src/main/java/com/example/ping_pong/Player.kt
+++ app/src/main/java/com/example/ping_pong/Player.kt
@@ -1,20 +1,25 @@
 package com.example.ping_pong
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 
-class MainActivity : AppCompatActivity() {
+class Player : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.player_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val imageView: ImageView = findViewById(R.id.imageView)
+        Glide.with(this).load(R.drawable.stick_bug_rick_roll).into(imageView)
     }
 }
